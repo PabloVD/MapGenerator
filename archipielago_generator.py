c353101f50f56f24c1fb166d14f0999f1fa419de
Pablo Villanueva Domingo <Pablo.Villanueva.Domingo@gmail.com>
2024-11-06 12:49:55 +0100

    last version

---
+++ archipielago_generator.py
@@ -0,0 +1,93 @@
+#---------------------------
+# Archipielago Generator
+# PabloVD
+# Started: 11/5/20
+#---------------------------
+
+"""
+---------------------------------------------------------------------------------
+Generate maps of random islands from a gaussian field.
+After being normalized and smoothed,
+only the mainland above a certain threshold is retained,
+being the rest considered as sea.
+The gaussian field is generated employing a cosmological package, powerbox,
+from a power spectrum as input.
+---------------------------------------------------------------------------------
+"""
+
+# TO DO
+# try with different noises: opensimplex, fractional brownian noise...
+# plot planet with spherical projection
+
+from source.visualization_tools import *
+
+#--- Parameters ---#
+
+# Number of bins per dimension
+boxsize = 500
+# Number of bins per dimension in the smoothed high resolution box
+highboxsize = boxsize
+# Threshold for the sea level
+threshold = 0.6
+# Sigma for the gaussian smoothing
+sigma = 5.
+# Initial random seed
+llavor = 0
+# Spectral index for the power spectrum (only for Gauss noise)
+indexlaw = -3.
+# Hurst index for Fractional Brownian Field
+hurst = 0.5
+# Choose kind of noise. See the source/field.py module for the options
+kind_noise = "gauss"
+# Include cities if 1
+include_cities = 0
+# Ensure that the boundaries are sea if 1
+make_island = 0
+
+#--- Perlin parameters ---#
+# Scale sets the size of the objects
+# Set to e.g. 500 to bigger portions of land
+scale = 500
+# Octaves is the number of levels, and increases the granularity with higher values
+# Lower values give less defined boundaries
+octaves = 6
+# Persistence determines how much each octave contributes to the overall shape (adjusts amplitude)
+# Lower than 1 means that sucessive octaves contribute less
+# Set to close to 1 to have more islands and less big structures
+persistence = 0.5
+# Lacunarity determines how much detail is added or removed at each octave (adjusts frequency)
+# More than 1 means that each octave will increase it’s level of fine grained detail (increased frequency)
+# For the octave i, the frecuency is lacunarity**i, and the amplitude is persistence**i
+lacunarity = 2.0
+
+if kind_noise == "gauss":
+    params = indexlaw
+elif kind_noise == "fbm":
+    params = hurst
+else:
+    params = [scale,octaves,persistence,lacunarity,boxsize]
+
+
+for llavor in range(100):
+    single_map(kind_noise,boxsize,llavor,params,sigma,threshold,cities=include_cities)
+
+#plot_grid(kind_noise,boxsize,params,sigma,threshold,num_plots=3,cities=include_cities,make_island=make_island)
+
+
+exit()
+
+#--- MAIN ---#
+
+# Generate maps for different sigmas and thresholds
+
+llavors = range(1)
+sigmas = [sigma]
+thresholds = [threshold]
+
+for llavor in llavors:
+    for sigma in sigmas:
+        for threshold in thresholds:
+            single_map(kind_noise,boxsize,llavor,params,sigma,threshold,cities=include_cities)
+
+
+plt.show()
---
+++ source/maps.py
@@ -0,0 +1,89 @@
+#---------------------------
+# Field generator module
+# PabloVD
+# Started: 11/5/20
+#---------------------------
+
+from scipy import interpolate, ndimage
+from source.fields import *
+
+# Filter the field with a gaussian window
+def smooth_field(field,sigmagauss,gridsize=None):
+
+    if gridsize==None: gridsize=field.shape[0]
+
+    x, y = np.linspace(0,field.shape[0],num=field.shape[0]), np.linspace(0,field.shape[1],num=field.shape[1])
+
+    # Interpolation
+    f = interpolate.interp2d(x,y,field,kind="linear")
+
+    qx = np.linspace(x[0],x[-1], num = gridsize)
+    qy = np.linspace(y[0],y[-1], num = gridsize)
+
+    # Filtering
+    smooth = ndimage.filters.gaussian_filter(f(qx,qy),sigmagauss)
+    return smooth
+
+# Remove regions below sea level
+def mainland(field,threshold):
+    for i, row in enumerate(field):
+        for j, el in enumerate(row):
+            if el<threshold:   field[i,j]=0.
+    return field
+
+# Normalize the values of the field between 0 and 1
+def normalize_field(field):
+    min, max = np.amin(field), np.amax(field)
+    newfield = (field-min)/(max-min)
+    return newfield
+
+# A gaussian function
+def central_gaussian(x,y,x_c,y_c,sig):
+    return np.exp(-((x-x_c)**2.+(y-y_c)**2.)/2./sig**2.)
+
+# Multiply the field by a gaussian mask to ensure an island in the center of the image
+def masked_field(field,sig=None):
+    a, b = field.shape[0], field.shape[1]
+    if sig==None: sig = a/2.
+    x, y = np.linspace(0,a-1,num=a), np.linspace(0,b-1,num=b)
+    X, Y = np.meshgrid(x,y)
+    mask = central_gaussian(X,Y,a/2,b/2,sig)
+    field = field*mask
+    return field
+
+# Generate a map of islands applying different processes:
+# 1. Generate a random field, either gaussian or perlin
+# 2. Normalize the field between 0 and 1
+# 3. Smooth the field with a gaussian filter
+# 4. Retain only the mainland above a certain threshold
+def generate_map(kind_noise,boxsize,llavor,params,sigma,threshold,boxsizey=None,make_island=0):
+
+    if boxsizey==None: boxsizey=boxsize
+    np.random.seed(seed=llavor)
+
+    if kind_noise=="gauss":
+        indexlaw = params
+        field = gaussian_field(boxsize,llavor,indexlaw)
+    elif kind_noise=="perlin":
+        scale,octaves,persistence,lacunarity,boxsizey = params
+        field = perlin_field(boxsize,llavor,scale,octaves,persistence,lacunarity,boxsizey=boxsizey)
+        #field = perlin_field(boxsize,llavor,*params)
+    elif kind_noise=="warped_perlin":
+        scale,octaves,persistence,lacunarity,boxsizey = params
+        field = warped_perlin_field(boxsize,llavor,scale,octaves,persistence,lacunarity,boxsizey=boxsizey)
+    elif kind_noise=="fbm":
+        hurst = params
+        field = brownian_surface(boxsize, H=hurst)
+    elif kind_noise=="cos":
+        scale,octaves,persistence,lacunarity,boxsizey = params
+        field = cos_field(boxsize,llavor,scale,octaves,persistence,lacunarity,boxsizey=boxsizey)
+    else:
+        print("Kind of noise not valid.")
+        return np.zeros((boxsize,boxsizey))
+
+    field = normalize_field(field)
+    if make_island:
+        field = masked_field(field)
+    field = smooth_field(field,sigma,gridsize=2*boxsize)
+    field = mainland(field,threshold)
+    return field
